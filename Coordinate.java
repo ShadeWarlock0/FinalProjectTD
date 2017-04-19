0833f8dd7db20a92481a6bde5a1700c8bac499b3
ShadeWarlock0 <colesbarmitzvah@yahoo.com>
2017-04-19 14:00:26 -0400

    Add files via upload

---
+++ Coordinate.java
@@ -0,0 +1,18 @@
+public class Coordinate
+{
+  private int xcoor;
+  private int ycoor;
+  public Coordinate(int x, int y)
+  {
+    xcoor = x;
+    ycoor = y;
+  }
+  public int getX()
+  {
+    return xcoor;
+  }
+  public int getY()
+  {
+    return ycoor;
+  }
+}
---
+++ EnemySprite.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.imageio.*;
+
+public class EnemySprite extends LivingSprite
+{
+  private int HP;
+  private ArrayList<Coordinate> path;
+  private int index;
+  
+  public EnemySprite(double left, double top, int width, int height, String image,
+                      double velocityX, double velocityY, int health, ArrayList<Coordinate> path)
+  {
+    super(left, top, width, height, image, 0, 0);
+    velocityX = 0;
+    velocityY = 0;
+    index = 0;
+    HP = health;
+    this.path = path;
+  }
+  public int getHP()
+  {
+    return HP;
+  }
+  public void injure(int damage)
+  {
+    HP -= damage;
+  }
+  public void step(World world)
+  {
+    super.step(world);
+    if (super.getLeft() > path.get(index).getX())
+    {
+      super.setLeft(super.getLeft() - 1);
+    }
+    else if (super.getLeft() + super.getWidth() < path.get(index).getX())
+    {
+      super.setLeft(super.getLeft() + 1);
+    }
+    if (super.getTop() > path.get(index).getY())
+    {
+      super.setTop(super.getTop() - 1);
+    }
+    else if (super.getTop() - super.getHeight() < path.get(index).getY())
+    {
+      super.setTop(super.getTop() + 1);
+    }
+  }
+}
